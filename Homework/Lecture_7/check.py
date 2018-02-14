9da433f10ea57fc958a17db8cc624473c9597aeb
vilenic <aosobolev@gmail.com>
2018-02-14 12:46:19 +0300

    New assignment in Lecture 7

---
+++ Homework/Lecture_7/check.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+
+
+def check(func):
+    def inner(*args):
+        print("-" * 50)
+        print('The folowing arguments were given: {}\n'.format([*args]))
+        result = func(*args)
+        for el in [*args]:
+            print(el, "is of type:", type(el), "\n")
+        print("The folowing result has been achieved: {}\n".format(result))
+        print("Result is of type:", type(result), "\n")
+        return result
+    return inner
+
+
+@check
+def foo(*args):
+    return [*args] * 2
+
+
+@check
+def multiply(x, y):
+    return x * y
+
+
+if __name__ == '__main__':
+
+    a = foo((1, 2), "aaa")
+
+    b = multiply(10, 40)
+
+
